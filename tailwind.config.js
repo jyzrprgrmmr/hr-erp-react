99b9392aeeaa4b7e84c20fd63a7a17563e1fc224
csulit <chrisgelosulit@gmail.com>
2021-06-15 10:32:02 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class' or false
+  theme: {
+    extend: {
+      screens: {
+        xs: '320px',
+      },
+      colors: {
+        primary: '#f99d3a',
+      },
+      fontFamily: {
+        pop: ['Poppins'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+};
