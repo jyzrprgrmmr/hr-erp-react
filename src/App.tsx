b823abf8196771fac54883954158d5dd000e0bbe
csulit <chrisgelosulit@gmail.com>
2021-06-16 12:20:41 +0800

    Jayzur please adopt

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-//import Layout from './components/layout';
-import Login from './pages/auth';
+import Layout from './components/layout';
+//import Login from './pages/auth';
 
 const App: React.FC = () => {
   return (
-    <Login />
-    //<Layout />
+    //<Login />
+    <Layout />
   );
 };
 
